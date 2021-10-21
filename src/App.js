5aacb8632c237dbb19cc5f34c2040ae5356b717e
MaheswariSubburaj <maheswari.ksubburaj@gmail.com>
2021-10-21 09:10:34 -0600

    1.Html in js.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
+function getTitle(title) {
+return title;
+}
+
 function App() {
 
   return (
     <div>
-      <h1>Connectus Global 2021</h1> 
-    </div>
-  );
-
-  }
+      <h1>Connectus Global {getTitle(2021)}</h1> 
+    
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+  </div>
+  );  
+}
 
 export default App;
